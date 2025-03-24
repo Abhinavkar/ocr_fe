5ecadb75b78395c7bdc3fba47522f1210c6da335
diganta mohanty <diganta.mohanty@vvdntech.in>
2025-03-24 10:32:50 +0530

    Changes in sidenav

--- ocr_fe/src/components/SideNav.jsx
+++ ocr_fe/src/components/SideNav.jsx
@@ -61,8 +61,8 @@ const SideNav = () => {
                     </Link>
                 </li>
                 <li>
-                    <Link to="/answerupload">
-                        <span><img src={DownloadPng} alt="Download" /></span>Answer Upload 
+                    <Link to="/result">
+                        <span><img src={DownloadPng} alt="Download" /></span>Result Download
                     </Link>
                 </li>
                 {/* {(user.is_admin || user.is_sub_admin) && (
